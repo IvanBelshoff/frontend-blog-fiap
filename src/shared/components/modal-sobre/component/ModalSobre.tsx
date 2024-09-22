d1ef5334040e7a4ee2b17b6eb6fd5dab235565cc
Robson Nishikawa <takeshinishikawa@gmail.com>
2024-09-21 23:02:02 -0300

    fix: included adjustes on modalSobre responsiveness

--- src/shared/components/modal-sobre/component/ModalSobre.tsx
+++ src/shared/components/modal-sobre/component/ModalSobre.tsx
@@ -6,6 +6,8 @@ import {
     Modal,
     Paper,
     Typography,
+    useMediaQuery,
+    useTheme,
 } from '@mui/material';
 
 import { IModalSobreProps } from '../../../interfaces';
@@ -13,7 +15,12 @@ import { IModalSobreProps } from '../../../interfaces';
 export const ModalSobre: React.FC<IModalSobreProps> = ({
     openModalSobre,
     aoClicarEmFecharModal }) => {
-
+        const theme = useTheme();
+        const smDown = useMediaQuery(theme.breakpoints.down("sm"));
+        const mdDown = useMediaQuery(theme.breakpoints.down("md"));
+        const xlDown = useMediaQuery(theme.breakpoints.down("xl"));
+        const xsDown = useMediaQuery(theme.breakpoints.down("xs"));
+      
     return (
         <Modal
             open={openModalSobre}
@@ -21,23 +28,23 @@ export const ModalSobre: React.FC<IModalSobreProps> = ({
             aria-describedby="modal-modal-description"
 
         >
-            <Box width='100%' height='100%' display="flex" alignItems='center' justifyContent='center'>
+            <Box width='100%' height='100%' display="flex" alignItems='center' justifyContent='center' >
 
-                <Box width='auto' justifyContent="center" component={Paper} elevation={2}  >
+                <Box  flexDirection={"column"} alignContent='center' textAlign={'center'} width={smDown? "80vw" : mdDown ? "60vw" : xlDown ? "40vw" : "30vw" } height={smDown? "25vh" : "20vh" } justifyContent="space-between" component={Paper} elevation={2}  >
 
 
 
                     <Box width='100%' height='auto' display="flex" flexDirection="column" alignItems='center' justifyContent='center' gap={2} padding={2}>
 
-                        <Typography variant='h4'>Sistema de Blog Fiap</Typography>
+                        <Typography variant='h5'>Sistema de Blog Fiap</Typography>
 
-                        <Typography variant='h6'>Versão: 1.0 BETA - 2024</Typography>
+                        <Typography variant='h7'>Versão: 1.0 BETA - 2024</Typography>
 
                     </Box>
 
                     <Divider variant='middle' />
 
-                    <Box padding={1} width='100%' display='flex' justifyContent='center'>
+                    <Box paddingTop={2} width='100%' display='flex' justifyContent='center'>
                         <Button
                             variant='text'
                             color='primary'
